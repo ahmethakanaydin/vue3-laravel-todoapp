fb884d4748d5a5e1cbf7e2e82f3504a12144d758
Ahmet Aydın <ahmet@hakanaydin.me>
2021-09-15 15:02:14 +0300

    yeni yollar eklendi

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('todos', [TodoController::class, 'index']);
-Route::post('store-todo', [TodoController::class, 'store']);
+//public routes
 Route::post('delete-todo',[TodoController::class, 'delete']);
 Route::post('todo/mark-as-done',[TodoController::class, 'markAsDone']);
 Route::post('todo/mark-as-undone',[TodoController::class, 'markAsUnDone']);
+Route::get('todos', [TodoController::class, 'index']);
+Route::post('store-todo', [TodoController::class, 'store']);
 
+Route::post('register',[AuthController::class, 'register']);
+
+Route::post('login', [AuthController::class, 'login']);
+
+//private routes
+/* Route::group(['middleware' => ['auth:sanctum']], function () {
+   
+}); */
