2fd91b831c036ddeced5b1d2d97eff67ea525774
Ahmet Aydın <ahmet@hakanaydin.me>
2021-09-21 22:15:15 +0300

    Navbar eklendi

--- vuejs3/src/views/Todos.vue
+++ vuejs3/src/views/Todos.vue
@@ -1,70 +1,74 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="mt-8 text-2xl">Todolar</h1>
+  <div>
+    <Navbar />
 
-    <div class="mt-3">
-      <div class="grid grid-cols-12 gap-4">
-        <div class="col-span-6 space-y-4 px-1" style="height: 500px">
-          <div
-            v-for="(todo, index) in state.todos"
-            :key="index"
-            class="p-8 bg-white shadow-md rounded flex items-center justify-between"
-            :class="{ 'bg-green-200': todo.done }"
-          >
-            <div>
-              <div>{{ todo.text }}</div>
-              <div class="text-gray-500 text-sm">
-                Eklendiği Tarih: {{ todo.created_at.slice(0, 10) }}
+    <div class="container mx-auto">
+      <h1 class="mt-8 text-2xl">Todolar</h1>
+
+      <div class="mt-3">
+        <div class="grid grid-cols-12 gap-4">
+          <div class="col-span-6 space-y-4 px-1" style="height: 500px">
+            <div
+              v-for="(todo, index) in state.todos"
+              :key="index"
+              class="p-8 bg-white shadow-md rounded flex items-center justify-between"
+              :class="{ 'bg-green-200': todo.done }"
+            >
+              <div>
+                <div>{{ todo.text }}</div>
+                <div class="text-gray-500 text-sm">
+                  Eklendiği Tarih: {{ todo.created_at.slice(0, 10) }}
+                </div>
+              </div>
+              <div class="space-x-2">
+                <button
+                  class="px-2 text-red-600"
+                  @click="removeTodo(todo)"
+                  title="Sil"
+                >
+                  &times;
+                </button>
+                <button
+                  v-if="!todo.done"
+                  class="px-2 text-green-600"
+                  @click="markAsDone(todo)"
+                  title="Yapıldı olarak işaretle"
+                >
+                  &check;
+                </button>
+                <button
+                  v-else
+                  class="px-2 text-orange-600"
+                  @click="markAsUndone(todo)"
+                  title="Yapılacak olarak işaretle"
+                >
+                  &#8630;
+                </button>
               </div>
             </div>
-            <div class="space-x-2">
-              <button
-                class="px-2 text-red-600"
-                @click="removeTodo(todo)"
-                title="Sil"
-              >
-                &times;
-              </button>
-              <button
-                v-if="!todo.done"
-                class="px-2 text-green-600"
-                @click="markAsDone(todo)"
-                title="Yapıldı olarak işaretle"
-              >
-                &check;
-              </button>
-              <button
-                v-else
-                class="px-2 text-orange-600"
-                @click="markAsUndone(todo)"
-                title="Yapılacak olarak işaretle"
-              >
-                &#8630;
-              </button>
+            <div
+              v-if="state.todos.length === 0"
+              class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm"
+            >
+              Henüz hiç todo eklenmedi.
             </div>
           </div>
-          <div
-            v-if="state.todos.length === 0"
-            class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm"
-          >
-            Henüz hiç todo eklenmedi.
-          </div>
-        </div>
 
-        <div class="col-span-6">
-          <div class="p-8 bg-white shadow-md rounded">
-            <h2 class="text-xl">Todo Ekle</h2>
-            <div class="row">
-              <div class="col-10">
-                <input
-                  type="text"
-                  v-model="state.todoText"
-                  @keydown.enter="addTodo"
-                  class="p-2 mt-4 border rounded w-full"
-                />
-              </div>
-              <div class="col-2 mt-4">
-                <button @click="addTodo" class="btn btn-primary">Ekle</button>
+          <div class="col-span-6">
+            <div class="p-8 bg-white shadow-md rounded">
+              <h2 class="text-xl">Todo Ekle</h2>
+              <div class="row">
+                <div class="col-10">
+                  <input
+                    type="text"
+                    v-model="state.todoText"
+                    @keydown.enter="addTodo"
+                    class="p-2 mt-4 border rounded w-full"
+                  />
+                </div>
+                <div class="col-2 mt-4">
+                  <button @click="addTodo" class="btn btn-primary">Ekle</button>
+                </div>
               </div>
             </div>
           </div>
@@ -77,8 +81,11 @@
 <script>
 import axios from "axios";
 import { defineComponent, reactive } from "vue";
-
+import Navbar from "@/components/Navbar";
 export default defineComponent({
+  components: {
+    Navbar,
+  },
   setup() {
     const state = reactive({
       todos: [],
