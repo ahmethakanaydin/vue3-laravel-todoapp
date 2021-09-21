ab533efc1dc5de81229f9ed225cabb5f4a378a6a
Ahmet Aydın <ahmet@hakanaydin.me>
2021-09-21 22:15:58 +0300

    navbar kaldırıldı

--- vuejs3/src/App.vue
+++ vuejs3/src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <nav class="bg-red-600 text-white">
-    <div class="container mx-auto">
-      <div class="flex items-center">
-        <router-link
-          @click="logout"
-          to="/"
-          class="inline-block px-16 py-4 transition-colors hover:bg-red-500 duration-300"
-          >Çıkış Yap</router-link
-        >
-      </div>
-    </div>
-  </nav>
   <router-view />
 </template>
 
+<script>
+export default {};
+</script>
+
 <style scoped>
-.router-link-exact-active {
-  background-color: #c03f3f;
+.active {
+  display: none;
 }
 </style>
